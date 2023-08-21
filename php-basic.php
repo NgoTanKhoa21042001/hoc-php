e309033009e6a58127923bfc4d91b3427ca4c120
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-08-21 16:06:50 +0700

    cài đặt php , khai báo

---
+++ php-basic.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP basic</title>
+</head>
+<body>
+    <h1>Lamf quen với php</h1>
+    <?php
+    // dòng này sẽ code php
+    $fullname = "Ngo Tan Khoa ";
+    echo $fullname;
+    echo "Xin chào";
+    ?>
+</body>
+</html>
