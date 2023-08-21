7006c5ae68a549ccf2aba4297a4e082a0ef1dfd0
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-08-21 16:56:31 +0700

    vòng lặp

--- php-basic.php
+++ php-basic.php
@@ -10,9 +10,33 @@
     <h1>Lamf quen với php</h1>
     <?php
     // dòng này sẽ code php
-    $fullname = "Ngo Tan Khoa ";
-    echo "Xin chào {$fullname}";
-    // echo "Xin chào ".$fullname
+    // $fullname = "Ngo Tan Khoa ";
+    // echo "Xin chào {$fullname}";
+    // // echo "Xin chào ".$fullname
+    // $a = 20;
+    // $b = 2;
+    // echo $a + $b;
+    // $is_login = true;
+    // if ($is_login == false) {
+    //     echo "Chúc mừng bạn login successfully";
+    // } else {
+    //     echo "Raats tiếc bạn login failed";
+
+    // }
+    // vòng lặp for
+
+    $n = 10;
+    $sum = 0;
+
+    for($i = 1; $i <= $n; $i++) {
+        // tổng tất cả số chẵn
+        if ($i % 2 == 0) {
+            $sum = $sum + $i;
+        }
+        // echo $i. "<br />";
+    }
+    echo $sum;
+
     ?>
 </body>
 </html>
